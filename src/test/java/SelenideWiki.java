a917b2f7b55cbb11a88953839991c1bf190d46ad
dariaagoskova <inamorattta@gmail.com>
2022-08-16 11:53:11 +0300

    Corrected as per assignment

--- src/test/java/SelenideWiki.java
+++ src/test/java/SelenideWiki.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.byLinkText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -18,8 +18,8 @@ public class SelenideWiki {
     void findJUnit5Test() {
        open("/selenide/selenide");
        $("#wiki-tab").click();
-       $("#wiki-body").shouldHave(text("Soft assertions"));
-       $("#wiki-body").find(byText("Soft assertions")).click();
-       $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
+       $(".markdown-body").shouldHave(text("Soft assertions"));
+       $(byLinkText("Soft assertions")).click();
+       $(".markdown-body").shouldHave(text("Using JUnit5 extend"));
     }
 }
